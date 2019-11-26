5932a5af9f693d26b247514a52c86df9edab784d
MIkko <miguel.prieto.velasco@gmail.com>
2019-11-26 17:49:32 +0100

    Get

--- src/Calle.java
+++ src/Calle.java
@@ -2,4 +2,24 @@ public class Calle {
     int via[]; //Almacena el número de vehículos en cada via de la calle
     int Pos;
 
+    public Calle(int[] via, int pos) {
+        this.via = via;
+        Pos = pos;
+    }
+
+    public int[] getVia() {
+        return via;
+    }
+
+    public int getPos() {
+        return Pos;
+    }
+
+    public void setVia(int[] via) {
+        this.via = via;
+    }
+
+    public void setPos(int pos) {
+        Pos = pos;
+    }
 }
