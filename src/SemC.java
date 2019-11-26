97ffa8ab86e0ded8664fd059b82bd6fec3e93b35
Unknown <ik.choo92@gmail.com>
2019-11-26 17:55:45 +0100

    fix2 semC

--- src/SemC.java
+++ src/SemC.java
@@ -1,5 +1,14 @@
 public class SemC {
     int MaxR;
+    int MaxV;
+
+	public int getMaxV() {
+		return MaxV;
+	}
+
+	public void setMaxV(int maxV) {
+		MaxV = maxV;
+	}
 
 	public int getMaxR() {
 		return MaxR;
