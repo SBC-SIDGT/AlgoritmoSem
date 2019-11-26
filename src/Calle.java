6b829825f961fb850cb9e7ac8bcf7989d15350c8
MIkko <miguel.prieto.velasco@gmail.com>
2019-11-26 18:17:32 +0100

    Get

--- src/Calle.java
+++ src/Calle.java
@@ -1,25 +1,32 @@
 public class Calle {
-    int via[]; //Almacena el número de vehículos en cada via de la calle
+    ArrayList<int> vias = new ArrayList<int>(); //Almacena el número de vehículos en cada via de la calle
     int Pos;
 
-    public Calle(int[] via, int pos) {
-        this.via = via;
+    public Calle(ArrayList<int> vias, int pos) {
+        this.vias = vias;
         Pos = pos;
     }
 
-    public int[] getVia() {
-        return via;
+    public ArrayList<int> getVias() {
+        return vias;
+    }
+
+    public void setVias(ArrayList<int> vias) {
+        this.vias = vias;
     }
 
     public int getPos() {
         return Pos;
     }
 
-    public void setVia(int[] via) {
-        this.via = via;
-    }
-
     public void setPos(int pos) {
         Pos = pos;
+    }
+
+    public int totalCoches(){
+        int result=0;
+        for(int x=0;x<al.size();x++)
+            result=al.get(x);
+        return result;
     }
 }
