2a2d20fa0c8d24b1bce617cb8b5b315fa1088671
Unknown <ik.choo92@gmail.com>
2019-12-03 18:41:46 +0100

    conexion externa
    fix1

--- src/ConexionExterna.java
+++ src/ConexionExterna.java
@@ -4,11 +4,17 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.Scanner;
+
+import org.apache.commons.io.IOUtils;
+
+import jdk.nashorn.api.scripting.JSObject;
+import jdk.nashorn.internal.parser.JSONParser;
 
 public class ConexionExterna {
 	
 	public HttpURLConnection abrirConexion()throws IOException {
-		URL url = new URL("https://my-json-server.typicode.com/typicode/demo");
+		URL url = new URL("https://my-json-server.typicode.com/typicode/demo/posts");
 		HttpURLConnection con = (HttpURLConnection) url.openConnection();
 		
 		return con;
@@ -18,17 +24,25 @@ public class ConexionExterna {
 	 * https://www.oracle.com/technetwork/es/articles/java/api-java-para-json-2251318-esa.html
 	 */
 	public void getDatos (HttpURLConnection con) throws IOException {
+		URL url = new URL("https://my-json-server.typicode.com/typicode/demo/posts");
 		con.setRequestMethod("GET");
 		con.setRequestProperty("Content-Type", "application/json");
-		BufferedReader in = new BufferedReader(
-				  new InputStreamReader(con.getInputStream()));
-		String inputLine;
-		StringBuffer content = new StringBuffer();
-		while ((inputLine = in.readLine()) != null) {
-			content.append(inputLine);
-		}
-		in.close();
-		System.out.println(in);
+		con.connect();
+		String inline="";
+		int responsecode = con.getResponseCode(); 
+		if(responsecode != 200)
+			System.out.println(responsecode);
+			else
+			{
+				Scanner sc = new Scanner(url.openStream());
+				while(sc.hasNext())
+				{
+					inline+=sc.nextLine();
+				}
+				System.out.println("\nJSON data in string format");
+				System.out.println(inline);
+				sc.close();
+			}
 	}
 	
 }
